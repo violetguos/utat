73abecd3100a49709b676cb27c8db8f07cad08a1
Violet Guo <yueguo598@gmail.com>
2015-07-04 14:14:29 -0400

    working test EKF code, plotting P

--- run.m
+++ run.m
@@ -1,10 +1,31 @@
-%fstate=12,x=2,P=3,hmeas=3,z=3,Q=3,R=2, P=1
-%P = ekf(fstate,x,P,hmeas,z,Q,R)
-
-%%%{
-%starting my own EKF maybe}%
 
 
-pPlus =1.1
-pTkMinusOne = pPlus
-kTk = pMinus
+
+
+
+n=3;      %number of state
+q=0.1;    %std of process 
+r=0.1;    %std of measurement
+Q=q^2*eye(n); % covariance of process
+R=r^2;        % covariance of measurement  
+f=@(x)[x(2);x(3);0.05*x(1)*(x(2)+x(3))];  % nonlinear state equations
+h=@(x)x(1);                               % measurement equation
+s=[0;0;1];                                % initial state
+x=s+q*randn(3,1); %initial state          % initial state with noise
+P = eye(n);                               % initial state covraiance
+N=20;                                     % total dynamic steps
+xV = zeros(n,N);          %estmate        % allocate memory
+sV = zeros(n,N);          %actual
+zV = zeros(1,N);
+for k=1:N
+  z = h(s) + r*randn;                     % measurments
+  sV(:,k)= s;                             % save actual state
+  zV(k)  = z;                             % save measurment
+  [x, P] = ekf(f,x,P,h,z,Q,R);            % ekf 
+  xV(:,k) = x;                            % save estimate
+  s = f(s) + q*randn(3,1);                % update process 
+end
+for k=1:3                                 % plot results
+  subplot(3,1,k)
+  plot(1:N, sV(k,:), '-', 1:N, xV(k,:), '--')
+end
